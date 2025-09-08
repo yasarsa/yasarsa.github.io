d73476978383b5055f69dc226312f3a921388a7b
Yaşar Anıl Sansak <yasar.sansak@vestel.com.tr>
2025-09-08 15:03:33 +0300

    dnd calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import styles from './App.module.css';
+import DND from './components/DND/DND';
+import Home from './components/Home/Home';
 
 export default function App() {
 
   return (
     <div className={styles.App}>
-      <p>APP</p>
+      {window.location.pathname === "/" && <Home />}
+      {window.location.pathname === "/dnd" && <DND />}
     </div>
 
 
