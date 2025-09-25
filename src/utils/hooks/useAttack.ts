27633f656262c05e53cc219db80f423af0df8f04
Yaşar Anıl Sansak <yasar.sansak@vestel.com.tr>
2025-09-25 13:48:01 +0300

    added delete func

--- src/utils/hooks/useAttack.ts
+++ src/utils/hooks/useAttack.ts
@@ -21,5 +21,14 @@ export default function useAttack() {
         }
     }, [setAllAttacks]);
 
-    return { addAttack, getAttacks, setAllAttacks };
+    const deleteAttack = useCallback((index: number) => {
+        const attacks = localStorage.getItem('attacks');
+        if (attacks) {
+            const attacksArray = JSON.parse(attacks) as IAttack[];
+            attacksArray.splice(index, 1);
+            setAllAttacks(attacksArray);
+        }
+    }, [setAllAttacks]);
+
+    return { addAttack, getAttacks, setAllAttacks, deleteAttack };
 }
